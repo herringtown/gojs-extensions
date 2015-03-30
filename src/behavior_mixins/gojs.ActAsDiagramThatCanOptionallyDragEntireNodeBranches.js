475c06ffb9e576ffe4a4078a68ee993cf1e8e048
Greg Herrington <greg.herrington@gmail.com>
2015-03-30 00:26:35 -0400

    removed direct reference to prototype in cases of multiple inheritance.

--- src/behavior_mixins/gojs.ActAsDiagramThatCanOptionallyDragEntireNodeBranches.js
+++ src/behavior_mixins/gojs.ActAsDiagramThatCanOptionallyDragEntireNodeBranches.js
@@ -17,12 +17,10 @@ if (!createActAsDiagramThatCanOptionallyDragEntireNodeBranches) {
 			debug           = typeof debug === "undefined" ? false : debug;
 
 			// "this" will usually be the diagram, as the usage is actAsDiagramThatCanOptionallyDragEntireNodeBranches.call(diagram);
-			var diagram = options.context.diagram || this;
-			var toolManager = diagram.toolManager;
-			var draggingTool = toolManager.draggingTool;
+			var diagram           = options.context.diagram || this;
+			var toolManager       = diagram.toolManager;
+			var draggingTool      = toolManager.draggingTool;
 			var dragSelectingTool = toolManager.dragSelectingTool;
-
-
 
 			var defaultConditionsThatTriggerEnablingOfDragTreeAreMet = function () {
 
@@ -34,6 +32,7 @@ if (!createActAsDiagramThatCanOptionallyDragEntireNodeBranches) {
 			var conditionsThatTriggerEnablingOfDragTreeAreMet = options.condition || defaultConditionsThatTriggerEnablingOfDragTreeAreMet;
 
 
+			var __toolManager_doMouseDown = toolManager.doMouseDown;
 			// on mousedown, check to see if the conditions for switching to dragsTree are met...
 			toolManager.doMouseDown = function () {
 
@@ -55,16 +54,21 @@ if (!createActAsDiagramThatCanOptionallyDragEntireNodeBranches) {
 				//toolManager.draggingTool.dragsTree = true;
 
 				// do regular toolManager stuff
-				go.ToolManager.prototype.doMouseDown.call(toolManager);
-
+				//go.ToolManager.prototype.doMouseDown.call(toolManager);
+				__toolManager_doMouseDown.call(toolManager);
 
 			};
 
+
+
+			var __draggingTool_mayCopy = draggingTool.mayCopy;
 			// make sure the default copying behavior is subordinate to this condition
 			draggingTool.mayCopy = function () {
 
 				// default rune
-				var mayCopy = go.DraggingTool.prototype.mayCopy.call(draggingTool);
+				// var mayCopy = go.DraggingTool.prototype.mayCopy.call(draggingTool);
+				var mayCopy = __draggingTool_mayCopy.call(draggingTool);
+
 
 				debug && $log.error(" >>> toolManager.draggingTool.mayCopy () > default > ", mayCopy);
 
@@ -80,11 +84,18 @@ if (!createActAsDiagramThatCanOptionallyDragEntireNodeBranches) {
 
 			};
 
+
+
+			// @note : b/c other tools may have overriden this behavior, we are NOT calling the prototype method, but a reference
+			//         to the current (possibly already overriden) method
+			var __draggingTool_canStart = draggingTool.canStart;
+
 			// make sure that dragging WILL activate if the drag tool is active and the dragTree conditions are met
 			draggingTool.canStart = function () {
 
 				// default behavior
-				var canStart = go.DraggingTool.prototype.canStart.call(draggingTool);
+				//var canStart = go.DraggingTool.prototype.canStart.call(draggingTool);
+				var canStart = 	__draggingTool_canStart.call(draggingTool);
 
 				debug && $log.error(" >>> toolManager.draggingTool.canStart () > default > ", canStart);
 
@@ -101,17 +112,21 @@ if (!createActAsDiagramThatCanOptionallyDragEntireNodeBranches) {
 			};
 
 
+
+			var __dragSelectingTool_canStart = dragSelectingTool.canStart;
+
 			// make sure that drag selecting WILL NOT activate if the drag tool is active and the dragTree conditions are met
 			dragSelectingTool.canStart = function () {
 
 				// default behavior
-				var canStart = go.DragSelectingTool.prototype.canStart.call(dragSelectingTool);
+				//var canStart = go.DragSelectingTool.prototype.canStart.call(dragSelectingTool);
+				var canStart = 	__dragSelectingTool_canStart.call(dragSelectingTool);
 
 				debug && $log.error(" >>> toolManager.dragSelectingTool.canStart () > default > ", canStart);
 
 				if (conditionsThatTriggerEnablingOfDragTreeAreMet()) {
 
-					$log.error("      > dragTree is activated, CAN_START(drag select) = FALSE...");
+					debug && $log.error("      > dragTree is activated, CAN_START(drag select) = FALSE...");
 
 					canStart = false;
 
